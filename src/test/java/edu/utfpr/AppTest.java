f22cd8e6d65d19453bcaf1103a1676b0aea1d235
Willian Massami Watanabe <wwatanabe@utfpr.edu.br>
2016-04-06 19:28:34 -0300

    First version of selenium script with no functionality at all.

---
+++ src/test/java/edu/utfpr/AppTest.java
@@ -0,0 +1,16 @@
+package edu.utfpr;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+@RunWith(JUnit4.class)
+public class AppTest {
+
+    @Test
+    public void testApp () {
+        Assert.assertTrue( true );
+    }
+
+}
