d17a22792a5c677f8759edcdf7886f020382a8f2
Willian Massami Watanabe <wwatanabe@utfpr.edu.br>
2016-04-07 15:27:23 -0300

    now working with css attributes being registered

--- src/main/java/edu/utfpr/App.java
+++ src/main/java/edu/utfpr/App.java
@@ -17,17 +17,34 @@ public class App {
         WebDriver driver = new FirefoxDriver();
         JavascriptExecutor executor = (JavascriptExecutor) driver;
         FileWriter writer = new FileWriter(new File("r.csv"));
+        List <WebElement> all_elements;
+
         driver.get("file:///home/willian/Dropbox/artigos/xbi-css/201302/1.html");
+        all_elements = driver.findElements(By.cssSelector("*"));
         executor.executeScript("window.elements = document.querySelectorAll('*');");
         writer.write(executor.executeScript(
             "window.styles_t = window.getComputedStyle(window.elements[0], null);" +
             "window.result = [];" +
             "for (var i in window.styles_t) {" +
-                "if (!Number.isInteger(i) && typeof window.styles_t[i] !== 'function')" +
+                "if (Number.isNaN(parseInt(i)) && typeof window.styles_t[i] !== 'function')" +
                     "window.result.push(i);" +
             "}" +
-            "return window.result.join(',')"
+            "return window.result.join('|')"
         ).toString());
+
+        for (int i = 0; i < all_elements.size(); i++) {
+            writer.write("\n");
+            writer.write(executor.executeScript(
+                "window.styles_t = window.getComputedStyle(window.elements[" + i + "], null);" +
+                "window.result = [];" +
+                "for (var i in window.styles_t) {" +
+                    "if (Number.isNaN(parseInt(i)) && typeof window.styles_t[i] !== 'function')" +
+                        "window.result.push(window.styles_t[i]);" +
+                "}" +
+                "return window.result.join('|')"
+            ).toString());
+        }
+
         writer.close();
     }
 }
