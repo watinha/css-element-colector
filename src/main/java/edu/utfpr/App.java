6bd035640013a4a32e1bc321447e2ed52fcaaaca
Willian Massami Watanabe <wwatanabe@utfpr.edu.br>
2016-05-18 20:14:09 -0300

    now appending multiple webpages in a single dataset

--- src/main/java/edu/utfpr/App.java
+++ src/main/java/edu/utfpr/App.java
@@ -25,10 +25,11 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 public class App {
 
     public static void crawl_and_capture_screens (String url, String css_attributes, FileWriter writer,
-            WebDriver driver, JavascriptExecutor executor) throws IOException, InterruptedException {
+            WebDriver driver, JavascriptExecutor executor, String folder, String filename, List <WebDriver> lista_drivers) throws IOException, InterruptedException {
         List <WebElement> all_elements;
         all_elements = driver.findElements(By.cssSelector("*"));
-        executor.executeScript("window.elements = document.querySelectorAll('*');" +
+        executor.executeScript("window.css_attributes = " + css_attributes + ";" +
+                               "window.elements = document.querySelectorAll('*');" +
                                "var reset_element = document.createElement('link');" +
                                "reset_element.type = 'text/css';" +
                                "reset_element.rel = 'stylesheet';" +
@@ -37,7 +38,7 @@ public class App {
 
         for (int i = 0; i < all_elements.size(); i++) {
             writer.write("\n");
-            writer.write(executor.executeScript(
+            writer.write(folder + "." + filename + "." + i + "\t" + executor.executeScript(
                 "window.styles_t = window.getComputedStyle(window.elements[" + i + "], null);" +
                 "window.result = [];" +
                 "for (var i in window.css_attributes) {" +
@@ -47,11 +48,6 @@ public class App {
                 "return window.result.join('\t')"
             ).toString());
         }
-        writer.close();
-
-        List <WebDriver> lista_drivers = new ArrayList <> ();
-        lista_drivers.add(new FirefoxDriver());
-        lista_drivers.add(new ChromeDriver());
 
         for (int driver_index = 0; driver_index < lista_drivers.size(); driver_index++) {
             lista_drivers.get(driver_index).get(url);
@@ -92,19 +88,23 @@ public class App {
                     }
                 }
 
-                File targetLocation = new File("data/" + i + "." + driver_index + ".png");
+                File targetLocation = new File("data/" + folder + "." + filename + "." + i + "." + driver_index + ".png");
                 ImageIO.write(targetScreenshot, "png", targetLocation);
             }
-            lista_drivers.get(driver_index).quit();
         }
     }
 
     public static void main(String[] args) throws IOException, InterruptedException {
         WebDriver driver = new FirefoxDriver();
+        List <WebDriver> lista_drivers = new ArrayList <> ();
+        lista_drivers.add(new FirefoxDriver());
+        lista_drivers.add(new ChromeDriver());
         JavascriptExecutor executor = (JavascriptExecutor) driver;
         FileWriter writer = new FileWriter(new File("data/elements.csv"));
         BufferedReader br = new BufferedReader(new FileReader("css-attributes-selection.txt"));
-        String url = "file:///home/willian/Dropbox/artigos/xbi-css/_201302/_1.html";
+        List <String> url_list = new ArrayList <> ();
+        url_list.add("file:///home/willian/Dropbox/artigos/xbi-css/_201302/_1.html");
+        url_list.add("file:///home/willian/Dropbox/artigos/xbi-css/201303/1.html");
         String css_attributes = "[",
                attr = br.readLine();
 
@@ -119,15 +119,21 @@ public class App {
         }
         br.close();
 
-        driver.get(url);
+        driver.get(url_list.get(0));
         driver.manage().window().maximize();
         writer.write("id\t" + executor.executeScript(
-            // "window.styles_t = window.getComputedStyle(window.elements[0], null);" +
             "window.css_attributes = " + css_attributes + ";" +
             "return window.css_attributes.join('\t')"
         ).toString());
 
-        App.crawl_and_capture_screens(url, css_attributes, writer, driver, executor);
+        for (String url : url_list) {
+            String filename = url.substring(url.lastIndexOf("/") + 1),
+                   folder = url.substring(url.lastIndexOf("/", url.lastIndexOf("/") - 1) + 1, url.lastIndexOf("/") - 1);
+            App.crawl_and_capture_screens(url, css_attributes, writer, driver, executor, folder, filename, lista_drivers);
+        }
+        writer.close();
         driver.quit();
+        for (WebDriver d : lista_drivers)
+            d.quit();
     }
 }
